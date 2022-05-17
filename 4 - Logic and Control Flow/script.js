c8f75e4cd1880cc8716bb52943842294b49736ad
Kiona Coats <kionacoats@gmail.com>
2022-05-16 22:13:25 -0500

    Learned Logic and Control Flow

---
+++ 4 - Logic and Control Flow/script.js
@@ -0,0 +1,26 @@
+// if, else if
+
+const age = 15;
+
+if (age > 18){
+    console.log('You may enter!');
+} else if (age === 18){
+    console.log('You just turned 18. Welcome!');
+} else {
+    console.log('Go home Roger!');
+}
+
+// While loop
+let i = 0;
+while (i < 5) {
+    console.log(i)
+    i++;
+}
+
+
+// For Loop
+// for([initialization];[condition];[final-expression])
+
+for(let i = 0; i < 10; i++){
+    console.log(i);
+}
