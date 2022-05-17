39514e6888b02f260eae4d509ec45c5d0a0cc665
Kiona Coats <kionacoats@gmail.com>
2022-05-16 21:42:26 -0500

    Moved notes to new folder (3 - Operators)

--- 2- Variables/script.js
+++ 2- Variables/script.js
@@ -57,32 +57,3 @@ console.log(date);
 // Dynamically Typed
 let num = 5;
 
-// Comparison Operators --> true/false
-// > < >= <= == !=
-const a = 5;
-const b = 5;
-
-console.log(a >= b);         
-console.log(a == b);
-console.log(a != b);
-
-// Strict Equality
-// Compares VALUES and DATA TYPES
-// returns true only when both are the same
-console.log(5 === "5"); 
-// Loose Equality
-// Doesn't compare DATA TYPES
-console.log (5 == "5"); 
-
-// Strict Inequality
-console.log(a !== b); 
-
-// Logical Operators
-// OR ||: At least one operand is true
-console.log(true || false); 
-// AND &&: All operands are true --> True 
-console.log (true && true);
-// NOT !: Reverses boolean operand
-console.log(!false);
-
-
