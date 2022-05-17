69c4120124fdee4d74450cbfda887bec2ad3e65d
Kiona Coats <kionacoats@gmail.com>
2022-05-16 21:25:22 -0500

    Learned Operators

--- 2- Variables/script.js
+++ 2- Variables/script.js
@@ -53,6 +53,36 @@ const date = new Date();
 console.log(date);
 
 // Statically Typed
-int num = 5;
+// int num = 5;
 // Dynamically Typed
 let num = 5;
+
+// Comparison Operators --> true/false
+// > < >= <= == !=
+const a = 5;
+const b = 5;
+
+console.log(a >= b);         
+console.log(a == b);
+console.log(a != b);
+
+// Strict Equality
+// Compares VALUES and DATA TYPES
+// returns true only when both are the same
+console.log(5 === "5"); 
+// Loose Equality
+// Doesn't compare DATA TYPES
+console.log (5 == "5"); 
+
+// Strict Inequality
+console.log(a !== b); 
+
+// Logical Operators
+// OR ||: At least one operand is true
+console.log(true || false); 
+// AND &&: All operands are true --> True 
+console.log (true && true);
+// NOT !: Reverses boolean operand
+console.log(!false);
+
+
